4916cc1da5787a3764ca8439309a5b1a2a9917df
Lavanya R <lra@softwareag.com>
2023-09-21 12:23:26 +0530

    Implement asset properties widget

---
+++ asset-properties-widget/common/asset-property-constant.ts
@@ -0,0 +1,9 @@
+export const defaultProperty = [
+{ c8y_JsonSchema:{properties :{name:{type: "string",label: "Name"}} }, name: 'name', label: "Name", type: "string", active: true, isEditable:true, isExistingProperty:true},
+{  c8y_JsonSchema:{properties :{id: {type: "string",label: "ID"}}}, name: 'id', label: "ID",  type: "string", active: true ,isEditable:false, isExistingProperty:true},
+{  c8y_JsonSchema:{properties :{type: {type: "string",label: "Type"}}},name: 'type', label: "Type",  type: "string", active: true,isEditable:false, isExistingProperty:true},
+];
+
+export const property = [{  c8y_JsonSchema:{properties :{owner: {type: "string",label: "Owner"}}},name: 'owner', label: "Owner",  type: "string", isEditable:false, isExistingProperty:true},
+{  c8y_JsonSchema:{properties :{lastUpdated: {type: "string",label: "Last updated"}}},name: 'lastUpdated', label: "Last updated",  type: "string", isEditable:false, isExistingProperty:true}
+];
