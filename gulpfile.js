3f2065f7dac18221b6e5ec55958b37912e40a27f
EUR\y508925 <Amarjyoti.Raj@softwareag.com>
2023-09-26 22:39:34 +0530

    update gulp file command.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
 const {src, dest, series} = require('gulp');
-const replace = require('gulp-replace');
 const zip = require('gulp-zip');
 const del = require('del');
 const pkgJson = require('./package.json');
 
 function clean() {
-    del(['dist']);
-   
-    return src(`node_modules/leaflet-draw/dist/leaflet.draw-src.css`)
-    .pipe(replace(/(\'images\/spritesheet.png\'\))/, `'./../${pkgJson.c8y.application.contextPath}@${pkgJson.version}/images/spritesheet.png') !important`))
-    .pipe(replace(/(\'images\/spritesheet-2x.png\'\))/, `'./../${pkgJson.c8y.application.contextPath}@${pkgJson.version}/images/spritesheet-2x.png') !important`))
-    .pipe(replace(/(\'images\/spritesheet.svg\'\))/, `'./../${pkgJson.c8y.application.contextPath}@${pkgJson.version}/images/spritesheet.svg') !important`))
-    .pipe(dest(`smart-map-settings-widget/component/map-dialog/`));
+    return del(['dist']);
 }
+
 const bundle = series(
     function createZip() {
         return src(`./dist/apps/${pkgJson.c8y.application.contextPath}/**/*`)
