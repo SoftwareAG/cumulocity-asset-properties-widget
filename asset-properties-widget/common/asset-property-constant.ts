1175ec43b9cf914de52225b9feda1ddf690c78d3
EUR\y508925 <Amarjyoti.Raj@softwareag.com>
2023-09-29 15:56:03 +0530

    enable asset name edit flag.

--- asset-properties-widget/common/asset-property-constant.ts
+++ asset-properties-widget/common/asset-property-constant.ts
@@ -1,5 +1,5 @@
 export const defaultProperty = [
-{ c8y_JsonSchema:{properties :{name:{type: "string",label: "Name"}} }, name: 'name', label: "Name", type: "string", active: true, isEditable:false, isExistingProperty:true},
+{ c8y_JsonSchema:{properties :{name:{type: "string",label: "Name"}} }, name: 'name', label: "Name", type: "string", active: true, isEditable:true, isExistingProperty:true},
 {  c8y_JsonSchema:{properties :{id: {type: "string",label: "ID"}}}, name: 'id', label: "ID",  type: "string", active: true ,isEditable:false, isExistingProperty:true},
 {  c8y_JsonSchema:{properties :{type: {type: "string",label: "Type"}}},name: 'type', label: "Type",  type: "string", active: true,isEditable:false, isExistingProperty:true},
 ];
