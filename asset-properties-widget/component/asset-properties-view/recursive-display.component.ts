1cde6bf70124e6ab147c0cd81c1e5a978676e755
Nam Nhat Pham <naph@softwareag.com>
2023-11-29 08:54:37 +0100

    CTM-971: add comments to recursive template

--- asset-properties-widget/component/asset-properties-view/recursive-display.component.ts
+++ asset-properties-widget/component/asset-properties-view/recursive-display.component.ts
@@ -3,19 +3,24 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-recursive-display',
   template: `
+    <!-- Check if the value is primitive (like string, number, etc.); if it is, display it directly -->
     <ng-container *ngIf="isPrimitive(value); else complexValue">
       {{ value }}
     </ng-container>
+    <!-- Define a template for complex values (like objects or arrays) -->
     <ng-template #complexValue>
       <div *ngIf="isArray(value)">
         <ul>
+          <!-- For each element in the array, recursively display it using the same component -->
           <li *ngFor="let item of value">
             <app-recursive-display [value]="item"></app-recursive-display>
           </li>
         </ul>
       </div>
+      <!-- If the value is an object, iterate over its properties -->
       <div *ngIf="isObject(value)">
         <div *ngFor="let key of objectKeys(value)">
+          <!-- Display the key and then recursively display its value -->
           {{ key }}:
           <app-recursive-display [value]="value[key]"></app-recursive-display>
         </div>
