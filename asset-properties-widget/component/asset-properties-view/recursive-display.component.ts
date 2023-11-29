729708e9552868f2b8c8b26fec5ecdf13c793462
Nam Nhat Pham <naph@softwareag.com>
2023-11-29 09:44:52 +0100

    CTM-971: add check no data to recursive template

--- asset-properties-widget/component/asset-properties-view/recursive-display.component.ts
+++ asset-properties-widget/component/asset-properties-view/recursive-display.component.ts
@@ -5,21 +5,39 @@ import { Component, Input } from '@angular/core';
   template: `
     <!-- Check if the value is primitive (like string, number, etc.); if it is, display it directly -->
     <ng-container *ngIf="isPrimitive(value); else complexValue">
-      {{ value }}
+        <ng-container *ngIf="value != null && value !== ''; else noDataPrimitive">
+            {{ value }}
+        </ng-container>
     </ng-container>
+
+    <!-- Display "No data" for null or empty primitive values -->
+    <ng-template #noDataPrimitive>
+      <div>No data</div>
+    </ng-template>
+    
     <!-- Define a template for complex values (like objects or arrays) -->
     <ng-template #complexValue>
       <div *ngIf="isArray(value)">
-        <ul>
-          <!-- For each element in the array, recursively display it using the same component -->
-          <li *ngFor="let item of value">
-            <app-recursive-display [value]="item"></app-recursive-display>
-          </li>
-        </ul>
+          <!-- Display "No data" if the array is empty -->
+          <ng-container *ngIf="value.length > 0; else noDataArray">
+            <ul>
+              <!-- For each element in the array, recursively display it using this component -->
+              <li *ngFor="let item of value">
+                <app-recursive-display [value]="item"></app-recursive-display>
+              </li>
+            </ul>
+          </ng-container>
       </div>
+
+      <ng-template #noDataArray>
+          <div class="alert alert-info text-center">
+              {{ "No Data" | translate }}
+          </div>
+      </ng-template>
+
       <!-- If the value is an object, iterate over its properties -->
       <div *ngIf="isObject(value)">
-        <div *ngFor="let key of objectKeys(value)">
+        <div class="small m-b-0 m-r-8 text-truncate" *ngFor="let key of objectKeys(value)">
           <!-- Display the key and then recursively display its value -->
           {{ key }}:
           <app-recursive-display [value]="value[key]"></app-recursive-display>
