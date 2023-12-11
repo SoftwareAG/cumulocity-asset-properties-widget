ed88c148f36981e15367843c74037112e1813d3e
EUR\y508925 <Amarjyoti.Raj@softwareag.com>
2023-12-11 12:06:13 +0530

    test cases modified.

--- asset-properties-widget/component/asset-properties-view/asset-properties-view.component.spec.ts
+++ asset-properties-widget/component/asset-properties-view/asset-properties-view.component.spec.ts
@@ -9,10 +9,14 @@ describe('AssetPropertiesViewComponent', () => {
   let moRealtimeServiceMock: any;
   let assetPropertiesServiceMock: any;
   let asset;
+  let customPropertyObjects;
+  let configCustomPropertyObjects;
 
   beforeEach(() => {
     inventoryMock = { detail: jest.fn() };
     moRealtimeServiceMock = { onUpdate$: jest.fn() };
+    assetPropertiesServiceMock = { getCustomProperties:jest.fn() };
+    jest.useFakeTimers();
 
     component = new AssetPropertiesViewComponent(
       inventoryMock,
@@ -41,25 +45,105 @@ describe('AssetPropertiesViewComponent', () => {
       dateTest1: date.toISOString(),
       dateTest2: '',
     } as any as IManagedObject;
+    configCustomPropertyObjects = [
+      {
+        name: 'color',
+        description: '',
+        label: 'Color',
+        type: 'c8y_JsonSchema',
+        id: '123',
+        c8y_IsAssetProperty: {},
+        c8y_Global: {},
+        c8y_JsonSchema: {
+          type: 'object',
+          properties: {
+            color: {
+              type: 'string',
+              minLength: 2,
+              maxLength: 3,
+              minLengthValidate: true,
+              maxLengthValidate: true
+            }
+          },
+          required: [],
+          key: 'color',
+          title: 'Color'
+        }
+      },
+      {
+        name: 'length',
+        description: '',
+        label: 'Length',
+        type: 'c8y_JsonSchema',
+        id: '1234',
+        c8y_IsAssetProperty: {},
+        c8y_Global: {},
+        c8y_JsonSchema: {
+          type: 'object',
+          properties: {
+            length: {
+              type: 'number',
+              minimum: 2,
+              maximum: 5,
+              requiredMaximum: true,
+              requiredMinimum: true
+            }
+          },
+          required: [],
+          key: 'length',
+          title: 'Length'
+        }
+      }
+    ];
+    customPropertyObjects = [
+      {
+        name: 'color',
+        description: '',
+        label: 'Color',
+        type: 'c8y_JsonSchema',
+        id: '123',
+        c8y_IsAssetProperty: {},
+        c8y_Global: {},
+        c8y_JsonSchema: {
+          type: 'object',
+          properties: {
+            color: {
+              type: 'string',
+              minLength: 2,
+              maxLength: 3,
+              minLengthValidate: true,
+              maxLengthValidate: true
+            }
+          },
+          required: [],
+          key: 'color',
+          title: 'Color'
+        }
+      }
+    ];
   });
 
   it('should exist', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get selected asset details', async () => {
+  it('should get selected asset details and validate selected asset properties', async () => {
     // given
-    component.config = { device: asset };
+    component.config = { device: asset,properties: configCustomPropertyObjects};
     jest
       .spyOn(inventoryMock, 'detail')
       .mockReturnValue(Promise.resolve({ data: asset }));
     jest.spyOn(moRealtimeServiceMock, 'onUpdate$').mockReturnValue(of(asset));
+    jest.spyOn(assetPropertiesServiceMock, 'getCustomProperties').mockReturnValue(customPropertyObjects);
 
     // when
     await component.ngOnInit();
 
     // then
+    jest.runAllTimers();
+    await Promise.resolve();
     expect(component.selectedAsset).toEqual(asset);
+    expect(component.config.properties).toEqual(customPropertyObjects);
     expect(component.isEmptyWidget).toBe(false);
   });
 
