2d37624612587177c493f7032335177002155ff0
EUR\y508925 <Amarjyoti.Raj@softwareag.com>
2024-01-03 13:05:16 +0530

    SDK upgrade from 1018.497.8 to 1018.510.0

--- asset-properties-widget/component/asset-properties-view/asset-properties/asset-properties.component.spec.ts
+++ asset-properties-widget/component/asset-properties-view/asset-properties/asset-properties.component.spec.ts
@@ -205,7 +205,7 @@ describe('AssetPropertiesComponent', () => {
         pipe: () => of(null)
       }
     } as any;
-    const clusterMap = {ngOnDestroy: jest.fn(), ngOnInit: jest.fn()} as any;
+    const clusterMap = {mapView:{map:{invalidateSize : jest.fn()}}} as any;
 
     it('should use correct labels on complex object', async () => {
       // given
